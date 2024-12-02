57fdd02fcb77cb549c58ff0f9ac82a336465777c
Dalvinderjit Singh <66400608+dlvndrjt@users.noreply.github.com>
2024-12-02 20:35:26 +0800

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import Link from "next/link"
 import { CreateListingDialog } from "@/components/create-listing-dialog"
 import { useRouter } from "next/navigation"
 
@@ -19,13 +18,13 @@ export function Navbar() {
         className="text-xl font-bold hover:underline p-0 text-white"
         onClick={() => handleNavigation("/home")}
       >
-        CAPOL
+        CAPOEP
       </Button>
       <div className="flex items-center space-x-4">
         <CreateListingDialog />
         <Button
           variant="outline"
-          className="border border-white text-white hover:bg-white hover:text-gray-800"
+          className="border border-white text-black hover:bg-white hover:text-gray-800"
           onClick={() => handleNavigation("/account")}
         >
           Account
