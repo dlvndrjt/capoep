fe901865877c7b0d7da948829b85013f8014ef83
Dalvinderjit Singh <66400608+dlvndrjt@users.noreply.github.com>
2024-12-05 22:49:25 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "../styles/globals.css";
 import { Navbar } from "@/components/navbar"
+import { ToastProvider } from "@/components/providers/toast-provider"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
         <main>{children}</main>
+        <ToastProvider />
       </body>
     </html>
   )
