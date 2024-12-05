4fbf417faadfa5f16fcd8db2a93eec7fa84149a4
Dalvinderjit Singh <66400608+dlvndrjt@users.noreply.github.com>
2024-12-05 22:50:50 +0800

    Update hardhat.config.ts

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+// const config: HardhatUserConfig = {
+//   solidity: "0.8.27",
+// };
+
+// export default config;
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: {
+    version: "0.8.27", // Keep your original version or update it if needed
+    settings: {
+      optimizer: {
+        enabled: true, // Enable the optimizer
+        runs: 200, // This specifies the optimization level. You can set this based on your needs (default is 200)
+      },
+    },
+  },
 };
 
 export default config;
