4a26d3603df15f8b44352cc7b76aa34225a9bf1c
Dalvinderjit Singh <66400608+dlvndrjt@users.noreply.github.com>
2024-12-07 21:59:55 +0800

    Update hardhat.config.ts

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -11,7 +11,7 @@ const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.27", // Keep your original version or update it if needed
     settings: {
-      viaIR: true,
+      viaIR: false,
       optimizer: {
         enabled: true, // Enable the optimizer
         runs: 200, // This specifies the optimization level. You can set this based on your needs (default is 200)
