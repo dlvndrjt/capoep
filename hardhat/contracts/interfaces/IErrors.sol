4aea93893d002c193b6fcc19506cc4b124b19ab3
Dalvinderjit Singh <66400608+dlvndrjt@users.noreply.github.com>
2024-12-09 11:59:22 +0800

    Create interface for errors

---
+++ hardhat/contracts/interfaces/IErrors.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+interface IErrors {
+    error ListingNotActive();
+    error UnauthorizedAccess();
+    // Add other common errors here
+}
