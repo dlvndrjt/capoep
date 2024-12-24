eb6dab98c02e5df2831bf19289e12638d8db6f8e
Dalvinderjit Singh <66400608+dlvndrjt@users.noreply.github.com>
2024-12-24 23:43:32 +0800

    Update Diamond.sol

--- hardhat/contracts/Diamond.sol
+++ hardhat/contracts/Diamond.sol
@@ -1,40 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-//******************************************************************************\
-//* Author: Nick Mudge <nick@perfectabstractions.com> (https://twitter.com/mudgen)
-//* EIP-2535 Diamonds: https://eips.ethereum.org/EIPS/eip-2535
-//*
-//* Implementation of a diamond.
-//******************************************************************************/
+/******************************************************************************\
+* Author: Nick Mudge <nick@perfectabstractions.com> (https://twitter.com/mudgen)
+* EIP-2535 Diamonds: https://eips.ethereum.org/EIPS/eip-2535
+*
+* Implementation of a diamond.
+/******************************************************************************/
 
-import {LibDiamond} from "./libraries/LibDiamond.sol";
-import {IDiamondCut} from "./interfaces/IDiamondCut.sol";
-import {IDiamondLoupe} from "./interfaces/IDiamondLoupe.sol";
-import {IERC173} from "./interfaces/IERC173.sol";
-import {IERC165} from "./interfaces/IERC165.sol";
+import { LibDiamond } from "./libraries/LibDiamond.sol";
+import { IDiamondCut } from "./interfaces/IDiamondCut.sol";
 
-// When no function exists for function called
-error FunctionNotFound(bytes4 _functionSelector);
+contract Diamond {    
 
-// This is used in diamond constructor
-// more arguments are added to this struct
-// this avoids stack too deep errors
-struct DiamondArgs {
-    address owner;
-    address init;
-    bytes initCalldata;
-}
+    constructor(address _contractOwner, address _diamondCutFacet) payable {        
+        LibDiamond.setContractOwner(_contractOwner);
 
-contract Diamond {
-    constructor(
-        IDiamondCut.FacetCut[] memory _diamondCut,
-        DiamondArgs memory _args
-    ) payable {
-        LibDiamond.setContractOwner(_args.owner);
-        LibDiamond.diamondCut(_diamondCut, _args.init, _args.initCalldata);
-
-        // Code can be added here to perform actions and set state variables.
+        // Add the diamondCut external function from the diamondCutFacet
+        IDiamondCut.FacetCut[] memory cut = new IDiamondCut.FacetCut[](1);
+        bytes4[] memory functionSelectors = new bytes4[](1);
+        functionSelectors[0] = IDiamondCut.diamondCut.selector;
+        cut[0] = IDiamondCut.FacetCut({
+            facetAddress: _diamondCutFacet, 
+            action: IDiamondCut.FacetCutAction.Add, 
+            functionSelectors: functionSelectors
+        });
+        LibDiamond.diamondCut(cut, address(0), "");        
     }
 
     // Find facet for function that is called and execute the
@@ -47,12 +38,8 @@ contract Diamond {
             ds.slot := position
         }
         // get facet from function selector
-        address facet = ds
-            .facetAddressAndSelectorPosition[msg.sig]
-            .facetAddress;
-        if (facet == address(0)) {
-            revert FunctionNotFound(msg.sig);
-        }
+        address facet = ds.selectorToFacetAndPosition[msg.sig].facetAddress;
+        require(facet != address(0), "Diamond: Function does not exist");
         // Execute external function from facet using delegatecall and return any value.
         assembly {
             // copy function selector and any arguments
@@ -63,12 +50,12 @@ contract Diamond {
             returndatacopy(0, 0, returndatasize())
             // return any return value or error back to the caller
             switch result
-            case 0 {
-                revert(0, returndatasize())
-            }
-            default {
-                return(0, returndatasize())
-            }
+                case 0 {
+                    revert(0, returndatasize())
+                }
+                default {
+                    return(0, returndatasize())
+                }
         }
     }
 
