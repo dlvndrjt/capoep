3dda68661b3437c2328f0849400bf7af337cb3b6
Dalvinderjit Singh <66400608+dlvndrjt@users.noreply.github.com>
2024-12-24 23:44:21 +0800

    Update LibAppStorage.sol

--- hardhat/contracts/libraries/LibAppStorage.sol
+++ hardhat/contracts/libraries/LibAppStorage.sol
@@ -1,97 +1,39 @@
-// Create a struct for each facet's variable (to subsection more then create struct for each function or each need  )
-
-// do not put a struct directly in a struct eg reputationStorage { bla{} } - unless its ok with not being upgradeable
-
-// below is a reference implementation of appstorage from eip2535 page
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Diamond Storage is particularly good for isolating or compartmenting state variables to specific 
-// facets or functionality. This is great for creating modular facets that can be understood as their 
-// own units and be added to diamonds. A diamond with a lot of functionality is well organized and 
-// understandable if each of its facets can be understood in isolation. Diamond Storage helps make that 
-// possible.
+// learned from: https://github.com/aavegotchi/aavegotchi-contracts/blob/master/contracts/Aavegotchi/libraries/LibAppStorage.sol
+// learned from: eip 2535 app storage reff implementation
 
-// However, you may want to share state variables specific to your application with facets that are specific 
-// to your application. It can get somewhat tedious to call a `diamondStorage()` function in every function 
-// that you want to access state variables. 
+uint256 constant EQUIPPED_WEARABLE_SLOTS = 16;
+uint256 constant NUMERIC_TRAITS_NUM = 6;
+uint256 constant TRAIT_BONUSES_NUM = 5;
+uint256 constant PORTAL_AAVEGOTCHIS_NUM = 10;
+// constants can be used in appstorage and structs outside app storage
 
-// `AppStorage` is a specialized version of Diamond Storage. It is a more convenient way to access 
-// application specific state variables that are shared among facets.
-
-// The pattern works in the following way:
-
-// 1. Define a struct called AppStorage that contains all the state variables specific to your application 
-//    and that you plan to share with different facets. Store AppStorage in a file. Any of your facets can 
-//    now import this file to access the state variables.
+// example of struct that would be used in a mapping etc within appstorage:
+// (do not create structs directly in appstorage)
+// struct Dimensions {
+//     uint8 x;
+//     uint8 y;
+//     uint8 width;
+//     uint8 height;
+// }
 
 struct AppStorage {
     uint256 secondVar;
     uint256 firstVar;
     uint256 lastVar;
     // add other state variables ...
+
+    // EntryFacet
+    
 }
-
-
-// 2. In a facet that imports the AppStorage struct declare an AppStorage state variable called `s`. 
-//    This should be the only state variable declared in the facet. 
-
-// 3. In your facet you can now access all the state variables in AppStorage by prepending state variables 
-//    with `s.`. Here is example code:
-
-
-// import { AppStorage } from "./LibAppStorage.sol";
-
-contract AFacet {
-    AppStorage internal s;
-
-    function sumVariables() external {
-        s.lastVar = s.firstVar + s.secondVar;
-    }
-
-    function getFirsVar() external view returns (uint256) {
-        return s.firstVar;
-    }
-
-    function setLastVar(uint256 _newValue) external {
-        s.lastVar = _newValue;
-    }
-}
-
-// Sharing AppStorage in another facet:
-
-// import { AppStorage } from "./LibAppStorage.sol";
-
-contract SomeOtherFacet {
-    AppStorage internal s;
-
-    function getLargerVar() external view returns (uint256) {
-        uint256 firstVar = s.firstVar;
-        uint256 secondVar = s.secondVar;
-        if(firstVar > secondVar) {
-            return firstVar;
-        }
-        else {
-            return secondVar;
-        }
-    }
-}
-
-// Using the 's.' prefix to access AppStorage is a nice convention because it makes state variables 
-// concise, easy to access, and it distinguishes state variables from local variables and prevents 
-// name clashes/shadowing with local variables and function names. It helps identify and make 
-// explicit state variables in a convenient and concise way. AppStorage can be used in regualar 
-// contracts as well as proxy contracts, diamonds, implementation contracts, Solidity libraries and 
-// facets.
-
-// Since `AppStorage s` is the first and only state variable declared in facets its position in 
-// contract storage is `0`. This fact can be used to access AppStorage in Solidity libraries using 
-// diamond storage access. Here's an example of that:
 
 library LibAppStorage {
-    function appStorage() internal pure returns (AppStorage storage ds) {    
-        assembly { ds.slot := 0 }
+    function appStorage() internal pure returns (AppStorage storage ds) {
+        assembly {
+            ds.slot := 0
+        }
     }
 
     function someFunction() internal {
@@ -101,16 +43,51 @@ library LibAppStorage {
     }
 }
 
-// `AppStorage s` can be declared as the one and only state variable in facets or it can be declared in a 
-// contract that facets inherit.
-
-// AppStorage won't work if state variables are declared outside of AppStorage and outside of Diamond Storage. 
-// It is a common error for a facet to inherit a contract that declares state variables outside AppStorage and 
-// Diamond Storage. This causes a misalignment of state variables.
-
-// One downside is that state variables can't be declared public in structs so getter functions can't 
-// automatically be created this way. But it can be nice to make your own getter functions for 
-// state variables because it is explicit.
-
-// The rules for upgrading AppStorage are the same for Diamond Storage. These rules can be found at 
-// the end of the file ./DiamondStorage.sol
+contract Modifiers {
+    // AppStorage internal s;
+    // modifier onlyAavegotchiOwner(uint256 _tokenId) {
+    //     require(LibMeta.msgSender() == s.aavegotchis[_tokenId].owner, "LibAppStorage: Only aavegotchi owner can call this function");
+    //     _;
+    // }
+    // modifier onlyUnlocked(uint256 _tokenId) {
+    //     require(s.aavegotchis[_tokenId].locked == false, "LibAppStorage: Only callable on unlocked Aavegotchis");
+    //     _;
+    // }
+    // modifier onlyLocked(uint256 _tokenId) {
+    //     require(s.aavegotchis[_tokenId].locked == true, "LibAppStorage: Only callable on locked Aavegotchis");
+    //     _;
+    // }
+    // modifier onlyOwner() {
+    //     LibDiamond.enforceIsContractOwner();
+    //     _;
+    // }
+    // modifier onlyDao() {
+    //     address sender = LibMeta.msgSender();
+    //     require(sender == s.dao, "Only DAO can call this function");
+    //     _;
+    // }
+    // modifier onlyDaoOrOwner() {
+    //     address sender = LibMeta.msgSender();
+    //     require(sender == s.dao || sender == LibDiamond.contractOwner(), "LibAppStorage: Do not have access");
+    //     _;
+    // }
+    // modifier onlyOwnerOrDaoOrGameManager() {
+    //     address sender = LibMeta.msgSender();
+    //     bool isGameManager = s.gameManagers[sender].limit != 0;
+    //     require(sender == s.dao || sender == LibDiamond.contractOwner() || isGameManager, "LibAppStorage: Do not have access");
+    //     _;
+    // }
+    // modifier onlyItemManager() {
+    //     address sender = LibMeta.msgSender();
+    //     require(s.itemManagers[sender] == true, "LibAppStorage: only an ItemManager can call this function");
+    //     _;
+    // }
+    // modifier onlyOwnerOrItemManager() {
+    //     address sender = LibMeta.msgSender();
+    //     require(
+    //         sender == LibDiamond.contractOwner() || s.itemManagers[sender] == true,
+    //         "LibAppStorage: only an Owner or ItemManager can call this function"
+    //     );
+    //     _;
+    // }
+}
