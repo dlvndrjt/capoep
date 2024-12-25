60fba22aeb8aeb02ed014c0df19d73ab648cf787
Dalvinderjit Singh <66400608+dlvndrjt@users.noreply.github.com>
2024-12-25 23:57:26 +0800

    Add and update User and Entry states

--- hardhat/contracts/libraries/LibAppStorage.sol
+++ hardhat/contracts/libraries/LibAppStorage.sol
@@ -4,29 +4,80 @@ pragma solidity ^0.8.0;
 // learned from: https://github.com/aavegotchi/aavegotchi-contracts/blob/master/contracts/Aavegotchi/libraries/LibAppStorage.sol
 // learned from: eip 2535 app storage reff implementation
 
-uint256 constant EQUIPPED_WEARABLE_SLOTS = 16;
-uint256 constant NUMERIC_TRAITS_NUM = 6;
-uint256 constant TRAIT_BONUSES_NUM = 5;
-uint256 constant PORTAL_AAVEGOTCHIS_NUM = 10;
+// CONSTANTS
+uint256 constant MIN_ATTESTATIONS_FOR_MINT = 2;
 // constants can be used in appstorage and structs outside app storage
 
-// example of struct that would be used in a mapping etc within appstorage:
 // (do not create structs directly in appstorage)
-// struct Dimensions {
-//     uint8 x;
-//     uint8 y;
-//     uint8 width;
-//     uint8 height;
-// }
 
+// USER STATES
+// The User struct which stores the details of each user
+struct User {
+    uint256 id; // The ID of the User
+    string name; // Name of the user
+    address walletAddress; // Wallet address of the user
+    uint256 registeredAt; // Timestamp of user registration
+    uint256 reputationScore; // Reputation score of the user
+    uint256[] createdEntries; // List of Entry IDs created by the user
+    uint256[] votedEntries; // List of Entry IDs the user has voted on. (contains voteDetails(vote+comment))
+    uint256[] commentedEntries; // List of Entry IDs the user has commented on
+    uint256[] votedOnCommentsEntries; // List of Entry IDs where the user has voted on comments
+}
+// USER STATES END
+
+// ENTRY STATES
+// Enum for Entry states
+enum EntryState {
+    Active,
+    Archived,
+    Minted
+}
+
+// Enum for Vote Types
+enum VoteType {
+    Attest,
+    Refute
+}
+
+// Struct to store vote details along with the comment
+struct VoteDetail {
+    VoteType voteType; // Type of the vote (Attest or Refute)
+    string voteComment; // The comment explaining the reason for the vote
+    uint256 votedAt; // Timestamp of vote
+}
+
+// The Entry struct which stores the details of each Entry
+struct Entry {
+    uint256 id; // The ID of the Entry
+    address creator; // The creator of the Entry
+    string title; // Title of the Entry
+    string details; // Details of the Entry
+    string[] proofs; // Proofs related to the Entry
+    string category; // Category of the Entry
+    uint256 createdAt; // Timestamp of Entry creation
+    EntryState state; // Current state of the Entry (Active, Archived, Minted)
+    VoteDetail[] votes; // Array of vote details (attest/refute + comment) for the Entry
+    uint256 totalAttestCount; // Total number of attest votes for the Entry
+    uint256 totalRefuteCount; // Total number of refute votes for the Entry
+    uint256 linkedToPreviousId; // Linked to a previous Entry if current Entry is made on top of an archived Entry
+    uint256 linkedToNewId; // Linked to a new Entry if current Entry is archived and a new Entry is made on top of it
+    string archiveNote; // Archive note when applicable
+}
+// ENTRY STATES END
+
+// The storage struct holds all necessary state variables for facets
 struct AppStorage {
-    uint256 secondVar;
-    uint256 firstVar;
-    uint256 lastVar;
-    // add other state variables ...
+    // ENTRY STORAGE
+    uint256 nextEntryId; // Counter for generating unique Entry IDs
+    mapping(uint256 => Entry) Entries; // Stores Entries by ID
+    mapping(bytes32 => bool) EntryHashes; // Prevents duplicate Entries
+    mapping(uint256 => uint256[]) versionHistory; // Stores version history for each Entry
+    // ENTRY STORAGE END
 
-    // EntryFacet
-    
+    // USER STORAGE
+    uint256 nextUserId; // Counter for generating unique User IDs
+    mapping(address => User) users; // User Address => User struct
+    // USER STORAGE END
 }
 
 library LibAppStorage {
@@ -38,56 +89,62 @@ library LibAppStorage {
 
     function someFunction() internal {
         AppStorage storage s = appStorage();
-        s.firstVar = 8;
+        // s.firstVar = 8;
         //... do more stuff
     }
 }
 
+// --- Modifiers ---
+
 contract Modifiers {
-    // AppStorage internal s;
-    // modifier onlyAavegotchiOwner(uint256 _tokenId) {
-    //     require(LibMeta.msgSender() == s.aavegotchis[_tokenId].owner, "LibAppStorage: Only aavegotchi owner can call this function");
-    //     _;
-    // }
-    // modifier onlyUnlocked(uint256 _tokenId) {
-    //     require(s.aavegotchis[_tokenId].locked == false, "LibAppStorage: Only callable on unlocked Aavegotchis");
-    //     _;
-    // }
-    // modifier onlyLocked(uint256 _tokenId) {
-    //     require(s.aavegotchis[_tokenId].locked == true, "LibAppStorage: Only callable on locked Aavegotchis");
-    //     _;
-    // }
-    // modifier onlyOwner() {
-    //     LibDiamond.enforceIsContractOwner();
-    //     _;
-    // }
-    // modifier onlyDao() {
-    //     address sender = LibMeta.msgSender();
-    //     require(sender == s.dao, "Only DAO can call this function");
-    //     _;
-    // }
-    // modifier onlyDaoOrOwner() {
-    //     address sender = LibMeta.msgSender();
-    //     require(sender == s.dao || sender == LibDiamond.contractOwner(), "LibAppStorage: Do not have access");
-    //     _;
-    // }
-    // modifier onlyOwnerOrDaoOrGameManager() {
-    //     address sender = LibMeta.msgSender();
-    //     bool isGameManager = s.gameManagers[sender].limit != 0;
-    //     require(sender == s.dao || sender == LibDiamond.contractOwner() || isGameManager, "LibAppStorage: Do not have access");
-    //     _;
-    // }
-    // modifier onlyItemManager() {
-    //     address sender = LibMeta.msgSender();
-    //     require(s.itemManagers[sender] == true, "LibAppStorage: only an ItemManager can call this function");
-    //     _;
-    // }
-    // modifier onlyOwnerOrItemManager() {
-    //     address sender = LibMeta.msgSender();
-    //     require(
-    //         sender == LibDiamond.contractOwner() || s.itemManagers[sender] == true,
-    //         "LibAppStorage: only an Owner or ItemManager can call this function"
-    //     );
-    //     _;
-    // }
+    AppStorage internal s;
+
+    modifier onlyVotingModule() {
+        require(
+            msg.sender == s.votingModule,
+            "Unauthorized: Not voting module"
+        );
+        _;
+    }
+
+    modifier onlyCapoep() {
+        require(msg.sender == s.capoepAddress, "Unauthorized: Not CAPOEP");
+        _;
+    }
+
+    modifier entryExists(uint256 entryId) {
+        require(entryId < s.nextEntryId, "Invalid entry ID");
+        _;
+    }
+
+    modifier onlyCreator(uint256 entryId) {
+        require(
+            msg.sender == s.Entries[entryId].creator,
+            "Unauthorized: Not entry creator"
+        );
+        _;
+    }
+
+    modifier onlyActive(uint256 entryId) {
+        require(
+            s.Entries[entryId].state == EntryState.Active,
+            "Invalid entry State: Not Active"
+        );
+        _;
+    }
+
+    modifier canEditentry(uint256 entryId) {
+        require(
+            entryId < s.nextEntryId &&
+                s.Entries[entryId].state == EntryState.Active &&
+                !hasVotes(entryId),
+            "Cannot edit entry: Invalid state or has votes"
+        );
+        _;
+    }
+
+    function hasVotes(uint256 entryId) internal view returns (bool) {
+        EntryCount memory counts = s.EntryCounts[entryId];
+        return counts.attestCount > 0 || counts.refuteCount > 0;
+    }
 }
